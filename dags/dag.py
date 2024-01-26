468d7e7b440fe5904c318a491cda793963fdb254
Sabrina Rodriguez <sabrina.rodriguez@yapo.cl>
2024-01-26 16:13:15 -0300

    chachge operator3

--- dags/dag.py
+++ dags/dag.py
@@ -3,8 +3,7 @@ import requests
 from datetime import datetime
 
 from airflow import models
-from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
-#from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator  # Importación actualizada
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators import python_operator
 
