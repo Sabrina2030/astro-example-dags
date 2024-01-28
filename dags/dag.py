1691432def705721ca543a3adbbf527d1f430fb2
Sabrina Rodriguez <sabrina.rodriguez@yapo.cl>
2024-01-28 16:02:56 -0300

    add projects

--- dags/dag.py
+++ dags/dag.py
@@ -1,81 +1,101 @@
-import json
-import requests
-from datetime import datetime
-
-from airflow import models
-#from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator  # Importación actualizada
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators import python_operator
-
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models.connection import Connection
+from time import time_ns
+from datetime import datetime , timedelta
+from airflow.utils.dates import days_ago
+import os
+from pymongo import MongoClient
+from pandas import DataFrame
+from google.cloud import bigquery
+import pandas as pd
 
 default_args = {
-    # The start_date describes when a DAG is valid / can be run. Set this to a
-    # fixed point in time rather than dynamically, since it is evaluated every
-    # time a DAG is parsed. See:
-    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    "start_date": datetime(2022, 7, 13),
+    'owner': 'Datapath',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
 }
 
-#endpoint 
-service_url = 'http://127.0.0.1:5000/run_process'
+def get_connect_mongo():
 
-SLACK_CONN_ID = "slack"
+    CONNECTION_STRING ="mongodb+srv://atlas:T6.HYX68T8Wr6nT@cluster0.enioytp.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(CONNECTION_STRING)
 
-dag_name = "composer_pipeline_data-content_ad-params"
-dag_tags = [
-    "production",
-    "ETL",
-    "schedule",
-    "dockerhost",
-    "legacy",
-    "git: legacy/data-content",
-    "input: dwh",
-    "input: blocket",
-    "output: dwh",
-]
-schedule_interval = "10 4 * * *"
-riskiness = "High"
-utility = (
-    "Ads Params Etl processes params from verticals as it would be cars, inmo and big sellers, "
-    "then store them in DWH as appended method so a historical data would be always "
-    "available."
-)
-
-
-"""def task_fail_slack_alert(context):
-    slack_msg = slack_msg_body(
-        context,
-        riskiness=riskiness,
-        utility=utility,
-    )
-    failed_alert = SlackWebhookOperator(
-        task_id="failed_job_alert",
-        http_conn_id=SLACK_CONN_ID,
-        message=slack_msg,
-        username="airflow",
-        dag=dag,
-    )
-    return failed_alert.execute(context=context)"""
-
-
-with models.DAG(
-    dag_name,
-    tags=dag_tags,
-    schedule_interval=None,
-    default_args=default_args,
-    max_active_runs=1,
-) as dag:
+    return client
     
-    def execute_ad_params():
-        response = requests.post(service_url)
-        data = response.json()
-        return data
+def start_process():
+    print(" INICIO EL PROCESO!")
 
-    run_content_ad_params = python_operator.PythonOperator(
-        task_id="task_run_content_ad_params",
-        python_callable=execute_ad_params,
-        provide_context=True,
-        dag=dag,
+def end_process():
+    print(" FIN DEL PROCESO!")
+
+def load_products():
+    print(f" INICIO LOAD PRODUCTS")
+    dbconnect = get_connect_mongo()
+    dbname=dbconnect["retail_db"]
+    collection_name = dbname["products"] 
+    products = collection_name.find({})
+    products_df = DataFrame(products)
+    dbconnect.close()
+    products_df['_id'] = products_df['_id'].astype(str)
+    products_df['product_description'] = products_df['product_description'].astype(str)
+    products_rows=len(products_df)
+    print(f" Se obtuvo  {products_rows}  Filas")
+    products_rows=len(products_df)
+    if products_rows>0 :
+        client = bigquery.Client(project='premium-guide-410714')
+        table_id =  "premium-guide-410714.dep_raw.products"
+        job_config = bigquery.LoadJobConfig(
+            schema=[
+                bigquery.SchemaField("_id", bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("product_id", bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("product_category_id", bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("product_name", bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("product_description", bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("product_price", bigquery.enums.SqlTypeNames.FLOAT),
+                bigquery.SchemaField("product_image", bigquery.enums.SqlTypeNames.STRING),
+            ],
+            write_disposition="WRITE_TRUNCATE",
+        )
+
+
+        job = client.load_table_from_dataframe(
+            products_df, table_id, job_config=job_config
+        )  
+        job.result()  # Wait for the job to complete.
+
+        table = client.get_table(table_id)  # Make an API request.
+        print(
+            "Loaded {} rows and {} columns to {}".format(
+                table.num_rows, len(table.schema), table_id
+            )
+        )
+    else : 
+        print('alerta no hay registros en la tabla productos')
+
+
+with DAG(
+    dag_id="load_project",
+    schedule="20 04 * * *", 
+    start_date=days_ago(2), 
+    default_args=default_args
+) as dag:
+    step_start = PythonOperator(
+        task_id='step_start_id',
+        python_callable=start_process,
+        dag=dag
     )
-
-    run_content_ad_params
+    step_load = PythonOperator(
+        task_id='load_products_id',
+        python_callable=load_products,
+        dag=dag
+    )
+    step_end = PythonOperator(
+        task_id='step_end_id',
+        python_callable=end_process,
+        dag=dag
+    )
+    step_start>>step_load>>step_end
