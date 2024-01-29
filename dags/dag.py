1c91c68dceb86bfb71a688a8d76e3e3722c2bf3a
Sabrina Rodriguez <sabrina.rodriguez@yapo.cl>
2024-01-29 15:50:27 -0300

    conver to int

--- dags/dag.py
+++ dags/dag.py
@@ -284,7 +284,7 @@ def load_departments():
     departments_df = pd.DataFrame(departments)
     client.close()
     
-    departments_df['department_id'] = departments_df['department_id'].apply(lambda x: np.int64(x))
+    departments_df['department_id'] = departments_df['department_id'].astype(str).apply(lambda x: np.int64(x))
 
     departments_rows = len(departments_df)
     print(f"Se obtuvo {departments_rows} Filas")
