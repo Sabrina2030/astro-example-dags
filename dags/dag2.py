3b651e3925235d80f3b351208441b4511fa58e0f
Sabrina Rodriguez <sabrina.rodriguez@yapo.cl>
2024-02-07 00:49:22 -0300

    cambio de fechas2

--- dags/dag2.py
+++ dags/dag2.py
@@ -12,51 +12,35 @@ from airflow.operators import bash_operator, python_operator
 from airflow.sensors.python import PythonSensor
 
 default_args = {
-    "start_date": datetime(2023, 11, 1),  # Fecha de inicio predeterminada
-    "end_date": datetime(2023, 11, 1),  # Fecha de fin predeterminada
-    # Otros argumentos por defecto...
+    # The start_date describes when a DAG is valid / can be run. Set this to a
+    # fixed point in time rather than dynamically, since it is evaluated every
+    # time a DAG is parsed. See:
+    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
+    "start_date": datetime(2022, 3, 15),
 }
 
 SERVICE_SENSOR_URL = "https://py-scp-pipelines-healthchek-nasdocrtnq-ue.a.run.app"
 SERVICE_URL = "https://py-scp-kpi-operacionales-nasdocrtnq-ue.a.run.app"
 
-# Define el DAG...
+# Define a DAG (directed acyclic graph) of tasks.
+# Any task you create within the context manager is automatically added to the
+# DAG object.
+# Calculate the date
 def get_date():
-    start_date = "{{ dag_run.conf['start_date'] if dag_run.conf and 'start_date' in dag_run.conf else execution_date }}"
-    end_date = "{{ dag_run.conf['end_date'] if dag_run.conf and 'end_date' in dag_run.conf else execution_date }}"
-    return {"start_date": start_date, "end_date": end_date}
+    #start_date = "{{ dag_run.conf['start_date'] if dag_run.conf and 'start_date' in dag_run.conf else False}}"
+    #end_date = "{{ dag_run.conf['end_date'] if dag_run.conf and 'end_date' in dag_run.conf else False}}"
+    start_date = "2023-11-01"
+    end_date = "2023-11-01"
+    if start_date and end_date:
+        date = {"start_date": start_date, "end_date": end_date}
+    else:
+        execution_date = "{{ dag_run.logical_date }}"
+        execution_date = execution_date - timedelta(days=1)
+        execution_date = execution_date.date().strftime('%Y-%m-%d')
+        date = {"start_date": execution_date, "end_date": execution_date}
+        print(date)
+    return date
 
-def check_partition(**kwargs):
-    test = requests.post(
-        f"{SERVICE_SENSOR_URL}/{kwargs['table']}",
-        json=kwargs['dates']
-    )
-
-    try:
-        content_json = test.json()
-        status_table = content_json.get("body", {}).get("status_table")
-        print(status_table)
-        if status_table == "OK":
-            return True
-        elif status_table == "FAILED":
-            msg = content_json.get("body", {}).get("msg")
-            if "doesn't exist" in msg:
-                print(f"Data doesn't exist: {msg}")
-                return False
-            else:
-                raise ValueError(f"Unexpected status message: {msg}")
-        else:
-            raise ValueError(f"Unexpected status: {status_table}")
-    except json.decoder.JSONDecodeError:
-        return False
-
-
-# Process data block
-def call_job(**kwargs):
-    logging.info("Calling rundeck job")
-    data = requests.get(SERVICE_URL + "/rundeck/call")
-    logging.info("Done")
-    return data.json()
 
 with models.DAG(
     "trigger_kpi_operacionales",
@@ -73,6 +57,38 @@ with models.DAG(
     default_args=default_args,
     max_active_runs=1,
 ) as dag:
+    
+    def check_partition(**kwargs):
+        test = requests.post(
+            f"{SERVICE_SENSOR_URL}/{kwargs['table']}",
+            json=kwargs['dates']
+        )
+
+        try:
+            content_json = test.json()
+            status_table = content_json.get("body", {}).get("status_table")
+            print(status_table)
+            if status_table == "OK":
+                return True
+            elif status_table == "FAILED":
+                msg = content_json.get("body", {}).get("msg")
+                if "doesn't exist" in msg:
+                    print(f"Data doesn't exist: {msg}")
+                    return False
+                else:
+                    raise ValueError(f"Unexpected status message: {msg}")
+            else:
+                raise ValueError(f"Unexpected status: {status_table}")
+        except json.decoder.JSONDecodeError:
+            return False
+
+
+    # Process data block
+    def call_job(**kwargs):
+        logging.info("Calling rundeck job")
+        data = requests.get(SERVICE_URL + "/rundeck/call")
+        logging.info("Done")
+        return data.json()
 
     start_job = bash_operator.BashOperator(
         task_id="Start", bash_command="echo Success."
