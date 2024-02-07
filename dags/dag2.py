0f59f18cd92012f038f0e4735e4517d8562b995e
Sabrina Rodriguez <sabrina.rodriguez@yapo.cl>
2024-02-07 00:24:48 -0300

    cambio de fechas

--- dags/dag2.py
+++ dags/dag2.py
@@ -12,8 +12,8 @@ from airflow.operators import bash_operator, python_operator
 from airflow.sensors.python import PythonSensor
 
 default_args = {
-    "start_date": datetime(2023, 10, 1),  # Fecha de inicio predeterminada
-    "end_date": datetime(2023, 10, 1),  # Fecha de fin predeterminada
+    "start_date": datetime(2023, 11, 1),  # Fecha de inicio predeterminada
+    "end_date": datetime(2023, 11, 1),  # Fecha de fin predeterminada
     # Otros argumentos por defecto...
 }
 
@@ -25,6 +25,38 @@ def get_date():
     start_date = "{{ dag_run.conf['start_date'] if dag_run.conf and 'start_date' in dag_run.conf else execution_date }}"
     end_date = "{{ dag_run.conf['end_date'] if dag_run.conf and 'end_date' in dag_run.conf else execution_date }}"
     return {"start_date": start_date, "end_date": end_date}
+
+def check_partition(**kwargs):
+    test = requests.post(
+        f"{SERVICE_SENSOR_URL}/{kwargs['table']}",
+        json=kwargs['dates']
+    )
+
+    try:
+        content_json = test.json()
+        status_table = content_json.get("body", {}).get("status_table")
+        print(status_table)
+        if status_table == "OK":
+            return True
+        elif status_table == "FAILED":
+            msg = content_json.get("body", {}).get("msg")
+            if "doesn't exist" in msg:
+                print(f"Data doesn't exist: {msg}")
+                return False
+            else:
+                raise ValueError(f"Unexpected status message: {msg}")
+        else:
+            raise ValueError(f"Unexpected status: {status_table}")
+    except json.decoder.JSONDecodeError:
+        return False
+
+
+# Process data block
+def call_job(**kwargs):
+    logging.info("Calling rundeck job")
+    data = requests.get(SERVICE_URL + "/rundeck/call")
+    logging.info("Done")
+    return data.json()
 
 with models.DAG(
     "trigger_kpi_operacionales",
@@ -41,8 +73,6 @@ with models.DAG(
     default_args=default_args,
     max_active_runs=1,
 ) as dag:
-
-    # Resto del DAG...
 
     start_job = bash_operator.BashOperator(
         task_id="Start", bash_command="echo Success."
@@ -63,7 +93,7 @@ with models.DAG(
             mode="reschedule",
             timeout=600,
             python_callable=check_partition,
-            op_kwargs={"table": table},
+            op_kwargs={"table": table, "dates": get_date()},
         )
 
         start_job >> check_table_partition_exists >> call_rundeck_job
